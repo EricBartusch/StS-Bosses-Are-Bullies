81f0a6c66a2bd20c3823e3a7b8a2491a9c145fe7
Eric Bartusch <ericbartusch8@gmail.com>
2021-06-18 21:08:13 -0500

    Small fixes

--- src/main/java/bossesAreBullies/Patches/AwakenedOneExtraDialogPatch.java
+++ src/main/java/bossesAreBullies/Patches/AwakenedOneExtraDialogPatch.java
@@ -8,8 +8,6 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.beyond.AwakenedOne;
-import com.megacrit.cardcrawl.monsters.beyond.Deca;
-import com.megacrit.cardcrawl.monsters.beyond.Donu;
 import com.megacrit.cardcrawl.monsters.exordium.Cultist;
 
 import static bossesAreBullies.BossesAreBullies.makeID;
@@ -24,10 +22,10 @@ public class AwakenedOneExtraDialogPatch {
     }
 
     @SpirePatch(clz = AwakenedOne.class, method = "takeTurn")
-    public static class DecaDialog {
+    public static class AwakenedOneDialog {
 
         @SpirePrefixPatch
-        public static void Prefix(Deca __instance) {
+        public static void Prefix(AwakenedOne __instance) {
             if (AwakenedOneExtraDialog.isFirstTurn.get(__instance) && (
                     (Settings.hasRubyKey && Settings.hasSapphireKey && !Settings.hasEmeraldKey) ||
                     (!Settings.hasRubyKey && Settings.hasSapphireKey && Settings.hasEmeraldKey) ||
@@ -52,58 +50,19 @@ public class AwakenedOneExtraDialogPatch {
                     AbstractDungeon.actionManager.addToBottom(new TalkAction(jeff, dialog[5], 2.0F, 2.0F));
                     AbstractDungeon.actionManager.addToBottom(new TalkAction(frank, dialog[6], 3.0F, 3.0F));
                 } else if (!jeff.isDeadOrEscaped()){
-                    AbstractDungeon.actionManager.addToBottom(new TalkAction(frank, dialog[7], 2.0F, 2.0F));
-                    AbstractDungeon.actionManager.addToBottom(new TalkAction(frank, dialog[9], 2.0F, 1.0F));
-                    AbstractDungeon.actionManager.addToBottom(new TalkAction(frank, dialog[10], 1.5F, 1.5F));
-                    AbstractDungeon.actionManager.addToBottom(new TalkAction(frank, dialog[11], 2.0F, 3.0F));
-                    AbstractDungeon.actionManager.addToBottom(new TalkAction(frank, dialog[12], 1.5F, 1.5F));
+                    AbstractDungeon.actionManager.addToBottom(new TalkAction(jeff, dialog[7], 2.0F, 2.0F));
+                    AbstractDungeon.actionManager.addToBottom(new TalkAction(jeff, dialog[9], 2.0F, 1.0F));
+                    AbstractDungeon.actionManager.addToBottom(new TalkAction(jeff, dialog[10], 1.5F, 1.5F));
+                    AbstractDungeon.actionManager.addToBottom(new TalkAction(jeff, dialog[11], 3.0F, 2.0F));
+                    AbstractDungeon.actionManager.addToBottom(new TalkAction(jeff, dialog[12], 1.5F, 1.5F));
                 } else if (!frank.isDeadOrEscaped()) {
                     AbstractDungeon.actionManager.addToBottom(new TalkAction(frank, dialog[8], 2.0F, 2.0F));
                     AbstractDungeon.actionManager.addToBottom(new TalkAction(frank, dialog[9], 2.0F, 1.0F));
                     AbstractDungeon.actionManager.addToBottom(new TalkAction(frank, dialog[10], 1.5F, 1.5F));
-                    AbstractDungeon.actionManager.addToBottom(new TalkAction(frank, dialog[11], 2.0F, 3.0F));
+                    AbstractDungeon.actionManager.addToBottom(new TalkAction(frank, dialog[11], 3.0F, 2.0F));
                     AbstractDungeon.actionManager.addToBottom(new TalkAction(frank, dialog[12], 1.5F, 1.5F));
                 } else {
-                    AbstractDungeon.actionManager.addToBottom(new TalkAction(frank, dialog[13], 3.0F, 3.0F));
-                }
-
-                if(wasFast) {
-                    Settings.FAST_MODE = true;
-                }
-            }
-        }
-    }
-
-    @SpirePatch(clz = Donu.class, method = SpirePatch.CLASS)
-    public static class DonuExtraDialog
-    {
-        public static SpireField<String[]> extraDonuDecaDialog = new SpireField(() -> CardCrawlGame.languagePack.getUIString(makeID("ExtraDonuDecaText")).TEXT);
-        public static SpireField<Boolean> isFirstTurn = new SpireField(() -> true);
-    }
-
-    @SpirePatch(clz = Donu.class, method = "takeTurn")
-    public static class DonuDialog {
-
-        @SpirePrefixPatch
-        public static void Prefix(Donu __instance) {
-            if (DonuExtraDialog.isFirstTurn.get(__instance) && (
-                    (Settings.hasRubyKey && Settings.hasSapphireKey && !Settings.hasEmeraldKey) ||
-                            (!Settings.hasRubyKey && Settings.hasSapphireKey && Settings.hasEmeraldKey) ||
-                            (Settings.hasRubyKey && !Settings.hasSapphireKey && Settings.hasEmeraldKey))
-                    ) {
-                DonuExtraDialog.isFirstTurn.set(__instance, false);
-                String[] dialog = DonuExtraDialog.extraDonuDecaDialog.get(__instance);
-                boolean wasFast = Settings.FAST_MODE;
-                if (Settings.FAST_MODE) {
-                    Settings.FAST_MODE = false;
-                }
-
-                Deca deca = (Deca)AbstractDungeon.getCurrRoom().monsters.monsters.get(0);
-                if(deca.isDeadOrEscaped()) {
-                    AbstractDungeon.actionManager.addToBottom(new TalkAction(__instance, dialog[10], 2.0F, 2.0F));
-                    AbstractDungeon.actionManager.addToBottom(new TalkAction(__instance, dialog[11], 2.0F, 2.0F));
-                    AbstractDungeon.actionManager.addToBottom(new TalkAction(__instance, dialog[12], 3.0F, 3.0F));
-                    AbstractDungeon.actionManager.addToBottom(new TalkAction(__instance, dialog[13], 2.0F, 3.0F));
+                    AbstractDungeon.actionManager.addToBottom(new TalkAction(__instance, dialog[13], 3.0F, 3.0F));
                 }
 
                 if(wasFast) {
